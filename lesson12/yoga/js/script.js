0c8222078185cf958cbf52e815b35074da01e5fe
Oleg Mykhailov <o.a.mykhailov@gmail.com>
2019-08-29 01:11:47 +0300

    Lesson 12. Calculator

--- lesson12/yoga/js/script.js
+++ lesson12/yoga/js/script.js
@@ -252,4 +252,46 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         }
     });
+
+    // Calc
+    let persons = document.querySelectorAll('.counter-block-input')[0],
+        restDays = document.querySelectorAll('.counter-block-input')[1],
+        place = document.getElementById('select'),
+        totalValue = document.getElementById('total'),
+        personSum = 0,
+        daySum = 0,
+        total = 0;
+    
+    totalValue.innerHTML = 0;
+
+    persons.addEventListener('change', function() {
+        personSum = +this.value;
+        total = (daySum + personSum) * 4000;
+
+        if (restDays.value == '' || persons.value == '') {
+            totalValue.innerHTML = 0;
+        } else {
+            totalValue.innerHTML = total;
+        }
+    });
+
+    restDays.addEventListener('change', function() {
+        daySum = +this.value;
+        total = (daySum + personSum) * 4000;
+
+        if (persons.value == '' || restDays.value == '') {
+            totalValue.innerHTML = 0;
+        } else {
+            totalValue.innerHTML = total;
+        }
+    });
+
+    place.addEventListener('change', function() {
+        if (restDays.value == '' || persons.value == '') {
+            totalValue.innerHTML = 0;
+        } else {
+            let a = total;
+            totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+        }
+    });
 });
